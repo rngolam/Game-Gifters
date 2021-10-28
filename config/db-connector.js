f6f804e8afcf6f61a18757d8520b608590a0e403
Richard Ngo-Lam <rngolam@gmail.com>
2021-10-28 14:50:54 -0500

    connect to ClearDB

--- config/db-connector.js
+++ config/db-connector.js
@@ -4,10 +4,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_ngolamr',
-    password        : '6872',
-    database        : 'cs340_ngolamr'
+    host            : 'us-cdbr-east-04.cleardb.com',
+    user            : 'bd7c1d1344a061',
+    password        : 'f7c34804',
+    database        : 'heroku_833f8f811c7ce79'
 })
 
 // Export it for use in our application
