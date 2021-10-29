c17d2a6ed10ff9d3e60d61768d20da2d222593ea
Sola <hello@bysola.co>
2021-10-28 20:59:08 -0700

    Updated js stuff

--- temp/script.js
+++ temp/script.js
@@ -1,38 +1,34 @@
-var addModal = document.getElementById("modalBox1");
-var updateModal = document.getElementById("modalBox2");
-var filterModal = document.getElementById("modalBox3");
-
 function addEntry() { // user clicks on action button
-    addModal.style.display = "block";
+    document.getElementById("modalBox1").style.display = "block";
 }
 
 function updateEntry() { // user clicks on action button
-    updateModal.style.display = "block";
+    document.getElementById("modalBox2").style.display = "block";
 }
 
 function filter() { // user clicks on action button
-    filterModal.style.display = "block";
+    document.getElementById("modalBox3").style.display = "block";
 }
 
 window.onclick = function(event) { // user clicks outside of (x)
-    if (event.target == addModal) {
-        addModal.style.display = "none";
-    } else if (event.target == updateModal) {
-        updateModal.style.display = "none";
-    } else if (event.target == filterModal) {
-        filterModal.style.display = "none";
+    if (event.target == document.getElementById("modalBox1")) {
+        document.getElementById("modalBox1").style.display = "none";
+    } else if (event.target == document.getElementById("modalBox2")) {
+        document.getElementById("modalBox2").style.display = "none";
+    } else if (event.target == document.getElementById("modalBox3")) {
+        document.getElementById("modalBox3").style.display = "none";
     } 
 }
 
 
 function close1() {
-    addModal.style.display = "none";
+    document.getElementById("modalBox1").style.display = "none";
 }
 
 function close2() {
-    updateModal.style.display = "none";
+    document.getElementById("modalBox2").style.display = "none";
 }
 
 function close3() {
-    filterModal.style.display = "none";
+    document.getElementById("modalBox3").style.display = "none";
 }
