d303fded371a422d293f5eead1c88a77f3a1410b
Sola <hello@bysola.co>
2021-10-28 19:36:34 -0700

    Have dummy pop-up boxes working for add, update, filter on employee.html

---
+++ temp/script.js
@@ -0,0 +1,37 @@
+var addModal = document.getElementById("modalBox1");
+var updateModal = document.getElementById("modalBox2");
+var filterModal = document.getElementById("modalBox3");
+
+function showAddModal() { // user clicks on action button
+    addModal.style.display = "block";
+}
+
+function showUpdateModal() { // user clicks on action button
+    updateModal.style.display = "block";
+}
+
+function filter() { // user clicks on action button
+    filterModal.style.display = "block";
+}
+
+window.onclick = function(event) { // user clicks outside of (x)
+    if (event.target == addModal) {
+        addModal.style.display = "none";
+    } else if (event.target == updateModal) {
+        updateModal.style.display = "none";
+    } else if (event.target == filterModal) {
+        filterModal.style.display = "none";
+    } 
+}
+
+function close1() {
+    addModal.style.display = "none";
+}
+
+function close2() {
+    updateModal.style.display = "none";
+}
+
+function close3() {
+    filterModal.style.display = "none";
+}
