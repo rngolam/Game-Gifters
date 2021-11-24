454edd035479c754853ae68b4aff696db4ad5b75
Richard Ngo-Lam <rngolam@gmail.com>
2021-11-24 07:44:53 -0600

    implement search feature for Wishes page

--- routes/wishes.js
+++ routes/wishes.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-function getWishes(res, db, context, complete) {
+function getWishes(req, res, db, context, complete) {
 
-    const select_query = `SELECT wish_id, game_id, games.title AS game_title, wished_by AS associated_employee_id,
-    employees.first_name, employees.last_name, DATE_FORMAT(date_wished, "%c/%e/%Y") AS date_wished_formatted, fulfilled
-    FROM wishes
-    INNER JOIN games ON wishes.game_id=games.app_id
-    INNER JOIN employees ON wishes.wished_by=employees.employee_id
-    ORDER BY fulfilled, date_wished;`;
+    let select_query;
+    let inserts;
 
-    db.pool.query(select_query, function(error, results, fields) {
+    // Search query was used
+    if (req.query) {
+        select_query = `SELECT wish_id, game_id, games.title AS game_title, wished_by AS associated_employee_id,
+        employees.first_name, employees.last_name, DATE_FORMAT(date_wished, "%c/%e/%Y") AS date_wished_formatted, fulfilled
+        FROM wishes
+        INNER JOIN games ON wishes.game_id=games.app_id
+        INNER JOIN employees ON wishes.wished_by=employees.employee_id
+        WHERE employees.first_name LIKE ?
+        AND employees.last_name LIKE ?
+        ORDER BY fulfilled, date_wished;`;
+
+        inserts = [(req.query.firstName || '') + '%', (req.query.lastName || '') + '%']
+    
+    // Otherwise SELECT all rows
+    } else {
+        select_query = `SELECT wish_id, game_id, games.title AS game_title, wished_by AS associated_employee_id,
+        employees.first_name, employees.last_name, DATE_FORMAT(date_wished, "%c/%e/%Y") AS date_wished_formatted, fulfilled
+        FROM wishes
+        INNER JOIN games ON wishes.game_id=games.app_id
+        INNER JOIN employees ON wishes.wished_by=employees.employee_id
+        ORDER BY fulfilled, date_wished;`;
+    }
+
+    db.pool.query(select_query, inserts, function(error, results, fields) {
 
         if (error) {
             console.log(error);
@@ -86,7 +105,7 @@ router.get('/', function(req, res) {
     let callbackCount = 0
     const context = {page_name: 'wishes'}
 
-    getWishes(res, db, context, complete);
+    getWishes(req, res, db, context, complete);
     getGames(res, db, context, complete);
     getEmployees(res, db, context, complete);
 
